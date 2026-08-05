8dbd26e229b0758f7c7005bfd34f9b636a8a9ceb
Juan Garcia <juanf.garcia.navas@gmail.com>
2026-08-05 14:54:51 +0200

    two blogs

--- image-slot.js
+++ image-slot.js
@@ -504,7 +504,7 @@
       root.innerHTML =
         '<style>' + stylesheet + '</style>' +
         '<div class="frame" part="frame">' +
-        '  <img part="image" alt="" draggable="false" style="display:none">' +
+        '  <img part="image" alt="" draggable="false" fetchpriority="high" decoding="async" style="display:none">' +
         '  <div class="empty" part="empty">' + icon +
         '    <div class="cap"></div>' +
         '    <div class="sub">or <u>browse files</u></div></div>' +
